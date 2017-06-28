6d6f280401e9e6ab2fac60b5bf49c85581df1cec
k-yamada <yamadakazu45@gmail.com>
2017-06-29 02:51:52 +0900

    Initial commit

---
+++ lib/ruboty/lgtmin.rb
@@ -0,0 +1,7 @@
+require "ruboty/lgtmin/version"
+require "ruboty/handlers/lgtmin"
+
+module Ruboty
+  module Lgtmin
+  end
+end
---
+++ lib/ruboty/lgtmin/version.rb
@@ -0,0 +1,5 @@
+module Ruboty
+  module Lgtmin
+    VERSION = "0.1.1"
+  end
+end
